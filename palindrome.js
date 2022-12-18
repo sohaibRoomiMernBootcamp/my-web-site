aea9f063d35d3f2f8d76b17e22430f633660a6b2
sohaib roomi <sohaibroomi.mernbootcamp@gmail.com>
2022-12-18 20:53:19 +0500

    message

--- palindrome.js
+++ palindrome.js
@@ -1,12 +1,17 @@
-const word = document.getElementById("word").innerHTML;
+const word = document.getElementById("word").value;
 const yes = document.getElementById("yes");
 const no = document.getElementById("no");
+const validaton = document.getElementById("validation");
 
 function checkPalindrome(word) {
-    for(let i = 0; i < (word.length) / 2; i++) {
+    if (word.length > 9) {
+        for(let i = 0; i < (word.length) / 2; i++) {
         if (word[i] === word[word.length - 1]) {
             return yes.innerHTML("It is a palindrome.");
         }
             return no.innerHTML("It is a not palindrome.");
     }
+    }
+    return validaton.innerHTML("Please Enter a 10 charachter word")
+    
 }
