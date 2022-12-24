b1a568784fa6a45c88c2a1f37c5d3a97d801497d
sohaibRoomiMernBootcamp <120918215+sohaibRoomiMernBootcamp@users.noreply.github.com>
2022-12-24 17:53:38 +0500

    Final with improved CSS and 10 character validation in JS File

--- palindrome.js
+++ palindrome.js
@@ -1,52 +1,17 @@
-<<<<<<< HEAD
-// const word = document.getElementById("word").innerHTML;
-// const yes = document.getElementById("yes");
-// const no = document.getElementById("no");
+function checkPalindrome() {
+    let word = document.getElementById("word").value;
 
-// function checkPalindrome(word) {
-//     for(let i = 0; i < (word.length) / 2; i++) {
-//         if (word[i] === word[word.length - 1]) {
-//             return yes.innerHTML("It is a palindrome.");
-//         }
-//             return no.innerHTML("It is a not palindrome.");
-//     }
-// }
+    if (word.length < 11) {
 
-const word = prompt("Enter a word: ");
-const word_1 = (word.length) / 2;
-const last_char = word.length - 1;
-
-if (word.length < 11) {
-    function checkPalindrome(word) {
-        for(let i = 0; i < word_1; i++) {
-            if (word[i] === word[last_char]) {
-                return "It is a palindrome.";
+        for (let i = 0; i < word.length / 2; i++) {
+            if (word[i] === word[word.length - 1 - i]) {
+                document.getElementById("comment").innerHTML = "It is a palindrome.";
+            } else {
+                document.getElementById("comment").innerHTML = "It is not a palindrome.";
             }
-                return "It is a not palindrome.";
-=======
-const word = document.getElementById("word").value;
-const yes = document.getElementById("yes");
-const no = document.getElementById("no");
-const validaton = document.getElementById("validation");
-
-function checkPalindrome(word) {
-    if (word.length > 9) {
-        for(let i = 0; i < (word.length) / 2; i++) {
-        if (word[i] === word[word.length - 1]) {
-            return yes.innerHTML("It is a palindrome.");
->>>>>>> aea9f063d35d3f2f8d76b17e22430f633660a6b2
         }
     }
-<<<<<<< HEAD
-    
-const answer = checkPalindrome(word);
-console.log(answer);
-
-} else {
-    console.log("Please Enter a word less than 10 charachters.");
-=======
+    else {
+        return document.getElementById("comment").innerHTML = "Please enter a 10 charachter word.";
     }
-    return validaton.innerHTML("Please Enter a 10 charachter word")
-    
->>>>>>> aea9f063d35d3f2f8d76b17e22430f633660a6b2
 }
